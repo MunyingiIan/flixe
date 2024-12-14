b895760a3a292a882d2574a0b72c2230c71c24f6
Munyingi Ian <munyingi.mike@gmail.com>
2024-12-14 19:46:11 +0300

    Edited comment

--- lib/core/player/movie_player.dart
+++ lib/core/player/movie_player.dart
@@ -19,7 +19,7 @@ class MoviePlayer extends StatefulWidget {
   State<MoviePlayer> createState() => _WebVideoPlayerState();
 }
 
-// COmment
+// Comment
 //
 
 class _WebVideoPlayerState extends State<MoviePlayer> {
